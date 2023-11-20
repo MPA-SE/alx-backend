7028a17bb772abc9bdb52288c3db97371870bf7c
MPA-SE <priscillamomodu@gmail.com>
2023-11-20 11:07:11 +0000

    new commit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-""" 0-app module """
-from flask import Flask
-from routes.routes_0 import app_routes
+"""Flask App."""
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.debug = True
 
-app.register_blueprint(app_routes)
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """Handles route."""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
